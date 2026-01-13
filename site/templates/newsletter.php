f439aaf67ba27466c7581bdaededf9dbb95f84b8
Jack MB <hi@jackmurraybrown.com>
2026-01-13 10:35:23 +0100

    fix newsletter bugs

--- site/templates/newsletter.php
+++ site/templates/newsletter.php
@@ -1,6 +1,6 @@
 <?php snippet('header') ?>
 
-<div class="pt-40 lg:pt-80 px-4 lg:px-10 h-screen">
+<div class="pt-40 lg:pt-80 px-4 lg:px-10 min-h-screen">
   <h1 class="blob before:bg-dark-blue mb-10">
     <?= $page->title() ?>
   </h1>
@@ -10,7 +10,7 @@
     <div class="mt-16">
       <h2 class="mb-6">Past Editions</h2>
       <ul class="space-y-4">
-        <?php foreach ($page->editions()->toStructure() as $edition): ?>
+        <?php foreach ($page->editions()->toStructure()->sortBy('date', 'desc') as $edition): ?>
           <li>
             <a href="<?= $edition->link() ?>" target="_blank" rel="noopener noreferrer" class="hover:underline">
               <?= $edition->title() ?><?= $edition->date()->toDate("F 'y") ?>
